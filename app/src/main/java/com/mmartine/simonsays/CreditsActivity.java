e07a2b87f8ba638ce4122f9458939d47cb7b1615
miguelmoliaga <miguel.mo1999@gmail.com>
2022-03-03 00:50:00 +0100

    Autoscroll acabado

--- app/src/main/java/com/mmartine/simonsays/CreditsActivity.java
+++ app/src/main/java/com/mmartine/simonsays/CreditsActivity.java
@@ -1,47 +1,68 @@
 package com.mmartine.simonsays;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSmoothScroller;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class CreditsActivity extends AppCompatActivity {
 
+    private static final float MILLISECONDS_PER_INCH = 1000f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-
         ArrayList<String> elementosSuperiores = new ArrayList<>();
-        ArrayList<String> elementosInferiores = new ArrayList<>();
-
-        inicializaElementosListado(elementosInferiores, elementosSuperiores);
-
+        inicializaElementosListado(elementosSuperiores);
         Adapter adaptadorSuperior = getAdaptador (R.id.rvSuperior, elementosSuperiores);
-
-        // comportamiento del botón de bajar elementos
-
     }
 
-    public void inicializaElementosListado(ArrayList<String> elementosInferiores, ArrayList<String> elementosSuperiores) {
-        int i = 0;
-        elementosSuperiores.add(i++ + " Amparo");
-        elementosSuperiores.add(i++ + " Manuel");
-        elementosSuperiores.add(i++ + " Rosa");
-        elementosSuperiores.add(i++ + " Davinia");
-        elementosSuperiores.add(i++ + " Ángel");
-        elementosSuperiores.add(i++ + " Óscar");
-        elementosSuperiores.add(i++ + " Nerea");
-        elementosSuperiores.add(i++ + " Silvia");
-        elementosSuperiores.add(i++ + " Estrella");
-        elementosSuperiores.add(i++ + " José");
-        elementosSuperiores.add(i++ + " Francisco");
-        elementosSuperiores.add(i++ + " Daniela");
-        elementosSuperiores.add(i++ + " Triana");
+    public void inicializaElementosListado(ArrayList<String> elementosSuperiores) {
+        elementosSuperiores.add(" Amparo");
+        elementosSuperiores.add(" Manuel");
+        elementosSuperiores.add(" Rosa");
+        elementosSuperiores.add(" Davinia");
+        elementosSuperiores.add(" Ángel");
+        elementosSuperiores.add(" Óscar");
+        elementosSuperiores.add(" Nerea");
+        elementosSuperiores.add(" Silvia");
+        elementosSuperiores.add(" Estrella");
+        elementosSuperiores.add(" José");
+        elementosSuperiores.add(" Francisco");
+        elementosSuperiores.add(" Daniela");
+        elementosSuperiores.add(" Triana");
+        elementosSuperiores.add(" Amparo");
+        elementosSuperiores.add(" Manuel");
+        elementosSuperiores.add(" Rosa");
+        elementosSuperiores.add(" Davinia");
+        elementosSuperiores.add(" Ángel");
+        elementosSuperiores.add(" Óscar");
+        elementosSuperiores.add(" Nerea");
+        elementosSuperiores.add(" Silvia");
+        elementosSuperiores.add(" Estrella");
+        elementosSuperiores.add(" José");
+        elementosSuperiores.add(" Francisco");
+        elementosSuperiores.add(" Daniela");
+        elementosSuperiores.add(" Triana");
+        elementosSuperiores.add(" Amparo");
+        elementosSuperiores.add(" Manuel");
+        elementosSuperiores.add(" Rosa");
+        elementosSuperiores.add(" Davinia");
+        elementosSuperiores.add(" Ángel");
+        elementosSuperiores.add(" Óscar");
+        elementosSuperiores.add(" Nerea");
+        elementosSuperiores.add(" Silvia");
+        elementosSuperiores.add(" Estrella");
+        elementosSuperiores.add(" José");
+        elementosSuperiores.add(" Francisco");
+        elementosSuperiores.add(" Daniela");
+        elementosSuperiores.add(" Triana");
     }
 
     public Adapter getAdaptador (int idRecyclerView, ArrayList<String> lista){
@@ -50,10 +71,17 @@ public class CreditsActivity extends AppCompatActivity {
         rv.setLayoutManager(managerLayout);
         Adapter adaptador = new Adapter(this, lista);
         rv.setAdapter(adaptador);
-
-        DividerItemDecoration decorador = new DividerItemDecoration(
-                rv.getContext(), managerLayout.getOrientation());
-        rv.addItemDecoration(decorador);
+        RecyclerView.SmoothScroller smoothScroller = new LinearSmoothScroller(this) {
+            @Override protected int getVerticalSnapPreference() {
+                return LinearSmoothScroller.SNAP_TO_START;
+            }
+            @Override
+            protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
+                return MILLISECONDS_PER_INCH / displayMetrics.densityDpi;
+            }
+        };
+        smoothScroller.setTargetPosition(lista.size() - 1);
+        managerLayout.startSmoothScroll(smoothScroller);
         return adaptador;
     }
 }
